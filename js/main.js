ee6838bdf27f60f2c589853328b7024a6b63523e
Andreas Bigger <abigger87@gmail.com>
2018-01-06 10:05:00 -0500

    Update and rename javascript.txt to main.js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+window.MgApi.SetApiKey('82977648-ead1-4bdf-8018-043453a82a42'); // This is called only once. Get your api key at https://memegenerator.net/api.
+// window.MgApi.SetCredentials('username', 'password'); This is optional, if the user of your app logs in.
+// Most functions can be called anonymously, without credentials.
+
+window.MgApi.Generators_Search('frog', 0, 24);
